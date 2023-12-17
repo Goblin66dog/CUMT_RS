0d885f2324bc21b451c805d8d9a51a9749b8de16
Vegio_CUMT_RS <974247568@qq.com>
2023-12-17 16:14:22 +0800

    2023.12.17
    修改dataset代码，更加简洁

--- Dataset.py
+++ Dataset.py
@@ -6,35 +6,35 @@ class Dataset:
         # 读取影像
         self.file_path = file_path
         self.data = gdal.Open(self.file_path)
-        # 读取图像行数
-        self.im_width = self.data.RasterXSize
-        # 读取图像列数
-        self.im_height = self.data.RasterYSize
+        # 读取图像列数(宽度)
+        self.cols = self.data.RasterXSize
+        # 读取图像行数(高度)
+        self.rows = self.data.RasterYSize
         # 地图投影信息
         self.proj = self.data.GetProjection()
         # 仿射变换参数
-        self.geotrans = self.data.GetGeoTransform()
-        self.min_x = self.geotrans[0]
-        self.max_y = self.geotrans[3]
-        self.pixel_width = self.geotrans[1]
-        self.pixel_height = self.geotrans[5]
-        self.max_x = self.min_x + (self.im_width * self.pixel_width)
-        self.min_y = self.max_y + (self.im_height * self.pixel_height)
+        self.trans = self.data.GetGeoTransform()
+        self.min_x = self.trans[0]
+        self.max_y = self.trans[3]
+        self.pixel_width = self.trans[1]
+        self.pixel_height = self.trans[5]
+        self.max_x = self.min_x + (self.cols * self.pixel_width)
+        self.min_y = self.max_y + (self.rows * self.pixel_height)
         # 读取图像栅格数据
-        self.data_array = self.data.ReadAsArray(0, 0, self.im_width, self.im_height)
+        self.array = self.data.ReadAsArray(0, 0, self.cols, self.rows)
 
         # 图像维度/波段
-        if len(self.data_array.shape) == 2:
-            self.im_bands = 1
+        if len(self.array.shape) == 2:
+            self.bands = 1
         else:
-            self.im_bands = self.data_array.shape[0]
+            self.bands = self.array.shape[0]
         # 判断栅格数据的类型
-        if 'int8' in self.data_array.dtype.name:
-            self.datatype = gdal.GDT_Byte
-        elif 'int16' in self.data_array.dtype.name:
-            self.datatype = gdal.GDT_UInt16
+        if 'int8' in self.array.dtype.name:
+            self.type = gdal.GDT_Byte
+        elif 'int16' in self.array.dtype.name:
+            self.type = gdal.GDT_UInt16
         else:
-            self.datatype = gdal.GDT_Float32
+            self.type = gdal.GDT_Float32
 
         # 释放内存，如果不释放，在arcgis，envi中打开该图像时会显示文件被占用
         del self.data
